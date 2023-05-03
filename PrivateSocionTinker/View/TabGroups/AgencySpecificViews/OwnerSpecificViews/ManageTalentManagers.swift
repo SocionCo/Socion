ca5181653d61de60c811d0090431b06614d8c192
dbiundo09 <biundo@bc.edu>
2023-05-03 01:28:49 -0400

    Switching Branches

---
+++ PrivateSocionTinker/View/TabGroups/AgencySpecificViews/OwnerSpecificViews/ManageTalentManagers.swift
@@ -0,0 +1,20 @@
+//
+//  ManageTalentManagers.swift
+//  PrivateSocionTinker
+//
+//  Created by Daniel Biundo on 5/3/23.
+//
+
+import SwiftUI
+
+struct ManageTalentManagers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ManageTalentManagers_Previews: PreviewProvider {
+    static var previews: some View {
+        ManageTalentManagers()
+    }
+}
